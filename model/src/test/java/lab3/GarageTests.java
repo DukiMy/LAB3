c00b165fe6df480e5b92a0aab253ce41f42cb2ac
DukiMy <durim.miziraj@gmail.com>
2026-02-15 21:38:52 +0100

    mk prettier

--- model/src/test/java/lab3/GarageTests.java
+++ model/src/test/java/lab3/GarageTests.java
@@ -43,11 +43,15 @@ public class GarageTests {
 
   @Test
   void freeSlotsDecreasesWhenLoadedUntilFull() {
-    Garage<Vehicle> g = track(new Garage<>(Vehicle.class, ORIGO, 2));
+    Garage<Vehicle> g = track(new Garage<>(
+      Vehicle.class,
+      ORIGO,
+      2)
+    );
+
     Vehicle v1 = track(new Volvo240());
     Vehicle v2 = track(new Saab95());
     Vehicle v3 = track(new Scania());
-
     v1.mutatePoint(0.1, 0.1);
     v2.mutatePoint(0.2, 0.2);
     v3.mutatePoint(0.3, 0.3);
@@ -72,10 +76,14 @@ public class GarageTests {
 
   @Test
   void loadsClosestVehicleInRange() {
-    Garage<Vehicle> g = track(new Garage<>(Vehicle.class, ORIGO, 5));
+    Garage<Vehicle> g = track(new Garage<Vehicle>(
+      Vehicle.class,
+      ORIGO,
+      5)
+    );
+
     Vehicle near = track(new Volvo240());
     Vehicle far  = track(new Saab95());
-
     near.mutatePoint(1.0, 1.0);
     far.mutatePoint(9.0, 0.0);
 
@@ -96,9 +104,13 @@ public class GarageTests {
 
   @Test
   void doesNotLoadOutOfRange() {
-    Garage<Vehicle> g = track(new Garage<>(Vehicle.class, ORIGO, 5));
-    Vehicle outOfRange = track(new Volvo240());
+    Garage<Vehicle> g = track(new Garage<Vehicle>(
+      Vehicle.class,
+      ORIGO,
+      5)
+    );
 
+    Vehicle outOfRange = track(new Volvo240());
     outOfRange.mutatePoint(100.0, 100.0);
     Point2D before = outOfRange.getPoint();
 
@@ -119,10 +131,14 @@ public class GarageTests {
 
   @Test
   void doesNotLoadSameVehicleTwice() {
-    Garage<Vehicle> g = track(new Garage<>(Vehicle.class, ORIGO, 2));
+    Garage<Vehicle> g = track(new Garage<>(
+      Vehicle.class,
+      ORIGO,
+      2)
+    );
+
     Vehicle v = track(new Saab95());
     v.mutatePoint(0.1, 0.1);
-
     g.load();
     assertEquals(g.getPoint(), v.getPoint());
     assertEquals(1, g.getFreeSlots());
@@ -137,15 +153,18 @@ public class GarageTests {
 
   @Test
   void unloadMovesVehicleToLeftOfGarage() {
-    Garage<Vehicle> g = track(new Garage<>(Vehicle.class, new Point2D.Double(10.0d, 20.0d), 2));
+    Garage<Vehicle> g = track(new Garage<>(
+      Vehicle.class,
+      new Point2D.Double(10.0d, 20.0d),
+      2)
+    );
+
     Vehicle v = track(new Volvo240());
     v.mutatePoint(10.1, 20.1);
-
     g.load();
     assertEquals(g.getPoint(), v.getPoint(), "Vehicle borde inte lastas.");
 
     g.unLoad(v);
-
     assertEquals(9.0, v.getX(), 1e-12);
     assertEquals(20.0, v.getY(), 1e-12);
     assertEquals(
@@ -157,13 +176,17 @@ public class GarageTests {
 
   @Test
   void unloadOfVehicleNotInGarageDoesNothing() {
-    Garage<Vehicle> g = track(new Garage<>(Vehicle.class, ORIGO, 2));
+    Garage<Vehicle> g = track(new Garage<>(
+      Vehicle.class,
+      ORIGO,
+      2)
+    );
+
     Vehicle v = track(new Volvo240());
     v.mutatePoint(3.0, 3.0);
-
     Point2D before = v.getPoint();
-    g.unLoad(v);
 
+    g.unLoad(v);
     assertEquals(
       before,
       v.getPoint(),
@@ -179,11 +202,14 @@ public class GarageTests {
 
   @Test
   void typeFilterLoadsOnlyVehiclesOfT() {
-    Garage<Saab95> saabGarage = track(new Garage<>(Saab95.class, ORIGO, 2));
+    Garage<Saab95> saabGarage = track(new Garage<>(
+      Saab95.class,
+      ORIGO,
+      2)
+    );
 
     Saab95 saab = track(new Saab95());
     Volvo240 volvo = track(new Volvo240());
-
     saab.mutatePoint(0.2, 0.2);
     volvo.mutatePoint(0.1, 0.1);
 
